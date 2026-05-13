ee8a27bfd80007f443b5c1801bc250fd13a76715
Pranamyashukla <pranamyashukla08@gmail.com>
2026-05-13 17:24:48 +0530

    Add files via upload

---
+++ day_20_uni.py
@@ -0,0 +1,49 @@
+# Base class
+class University:
+    def __init__(self, name):
+        self.name = name
+
+
+class StudentDashboard(University):
+    def __init__(self, name, subjects, assignments):
+        super().__init__(name)
+        self.subjects = subjects
+        self.assignments = assignments
+
+    def display(self):
+        print("----- Student Dashboard -----")
+        print(f"Student Name: {self.name}")
+        print(f"Subjects: {self.subjects}")
+        print(f"Assignments: {self.assignments}")
+
+
+class TeacherDashboard(University):
+    def __init__(self, name, subjects, assignments):
+        super().__init__(name)
+        self.subjects = subjects
+        self.assignments = assignments
+
+    def display(self):
+        print("----- Teacher Dashboard -----")
+        print(f"Teacher Name: {self.name}")
+        print(f"Subjects Teaching: {self.subjects}")
+        print(f"Assignments Given: {self.assignments}")
+
+
+s1 = StudentDashboard(
+    "Rahul",
+    ["Maths", "Physics", "Computer Science"],
+    ["Math Assignment", "Python Project"]
+)
+
+t1 = TeacherDashboard(
+    "Mrs Sharma",
+    ["Computer Science", "AI"],
+    ["OOP Worksheet", "AI Research Task"]
+)
+
+s1.display()
+
+print()
+
+t1.display()
